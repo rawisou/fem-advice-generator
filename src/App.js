734f31e291f997abe6b279d4b484f859f313d3d1
Rawisara Saman <rawiisara@gmail.com>
2022-11-05 17:31:23 +0100

    basic style added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './style.css';
 
+
 const App = () => {
 
   const [advice, setAdvice] = useState([])
@@ -8,20 +9,25 @@ const App = () => {
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice")
       .then(res => res.json())
-      .then(data => setAdvice(data.slip))   
+      .then(data => setAdvice(data.slip))
   }, [])
 
   return (
-    <main>
-    <section>
-      <p>Advice no.# {advice.id}</p>
-      <p>{advice.advice}</p>
-    </section>
-    <footer>
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-    Coded by <a href="https://www.github.com/rawisou">Rawisara Saman</a>.
-    </footer>
-    </main>
+    <>
+      <main>
+        <section className="wrapper">
+          <div className="advice">
+            <p>Advice no.# {advice.id}</p>
+            <p>{advice.advice}</p>
+            <img src="./images/icon-dice.svg" alt="button"/>
+          </div>
+        </section>
+      </main>
+      <footer>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+        Coded by <a href="https://www.github.com/rawisou">Rawisara Saman</a>.
+      </footer>
+    </>
   )
 }
 
