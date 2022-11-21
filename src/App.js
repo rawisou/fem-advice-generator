447ff38e4d837f0b2a40f99b34882b1ebcf5320e
Rawisara Saman <rawiisara@gmail.com>
2022-11-21 19:18:53 +0100

    colors + pattern dividers added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './style.css';
 const App = () => {
 
   const [advice, setAdvice] = useState([])
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
 
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice")
@@ -12,15 +13,29 @@ const App = () => {
       .then(data => setAdvice(data.slip))
   }, [])
 
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 900);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <>
       <main>
         <div className="wrapper">
           <div className="advice">
             <p>Advice no.# {advice.id}</p>
-            <p>{advice.advice}</p>
+            <p>"{advice.advice}"</p>
+            {isDesktop ?
+              <img src="./images/pattern-divider-desktop.svg" alt="pattern divider desktop" /> 
+              :
+              <img src="./images/pattern-divider-mobile.svg" alt="pattern divider mobile" />
+            }
             <div className="dice-round">
-              <img src="./images/icon-dice.svg" alt="button"/>
+              <img src="./images/icon-dice.svg" alt="button" />
             </div>
           </div>
         </div>
