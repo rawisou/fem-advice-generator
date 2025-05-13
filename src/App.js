398ea432921711ebe5a876be1174943d6bb5fb04
Rawisara Saman <rawiisara@gmail.com>
2025-05-13 18:06:03 +0200

    finished up CSS and removed states that hold responsive info

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './style.css';
 const App = () => {
 
   const [advice, setAdvice] = useState([])
-  const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
+  // const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
 
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice")
@@ -13,27 +13,25 @@ const App = () => {
       .then(data => setAdvice(data.slip))
   }, [])
 
-  const updateMedia = () => {
-    setDesktop(window.innerWidth > 900);
-  };
+  // const updateMedia = () => {
+  //   setDesktop(window.innerWidth > 900);
+  // };
 
-  useEffect(() => {
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  });
+  // useEffect(() => {
+  //   window.addEventListener("resize", updateMedia);
+  //   return () => window.removeEventListener("resize", updateMedia);
+  // });
 
   return (
     <>
       <main>
         <div className="wrapper">
           <div className="advice">
-            <p>Advice no.# {advice.id}</p>
+            <p>ADVICE NO.# {advice.id}</p>
             <p>"{advice.advice}"</p>
-            {isDesktop ?
-              <img src="./images/pattern-divider-desktop.svg" alt="pattern divider desktop" /> 
-              :
-              <img src="./images/pattern-divider-mobile.svg" alt="pattern divider mobile" />
-            }
+
+            <img src="./images/pattern-divider-mobile.svg" alt="pattern divider mobile" />
+
             <div className="dice-round">
               <img src="./images/icon-dice.svg" alt="button" />
             </div>
@@ -41,8 +39,8 @@ const App = () => {
         </div>
       </main>
       <footer>
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-        Coded by <a href="https://www.github.com/rawisou">Rawisara Saman</a>.
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"> Frontend Mentor</a></p>.
+
       </footer>
     </>
   )
